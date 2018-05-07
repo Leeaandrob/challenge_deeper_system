2efff72b31d35bddc86d9db1eb665326c2bae190
Leandro Barbosa <leandrobar93@gmail.com>
2018-05-07 20:00:35 -0300

    adding tsts and view PopularThemesListView

--- project/videos/views.py
+++ project/videos/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from videos.models import Video
+from videos.utils import VideoInsights, PopularThemes
+
+
+class PopularThemesListView(ListView):
+    model = Video
+    template_name = 'videos/popular_themes.html'
+    context_object_name = 'videos'
+
+    def get_themes(self, videos):
+        data = [dict(
+            video=video.id,
+            themes=[dict(
+                id=theme.id,
+                name=theme.name,
+            ) for theme in video.themes.all()],
+            score=VideoInsights(video).get_score(),
+        ) for video in videos]
+
+        manager = PopularThemes(data)
+        return manager.get_popular_themes()
+
+    def get_context_data(self, **kwargs):
+        context = super(PopularThemesListView, self).get_context_data(**kwargs)
+        context['themes'] = self.get_themes(context['videos'])
+        return context
